b8219070a6e20f17e159ba17a7b9b6404b237ecd
Fuzail Shahzad <uf7@njit.edu>
2024-03-01 19:13:36 -0500

    Phase 2 Final Push - FS

--- database_error.php
+++ database_error.php
@@ -1,3 +1,4 @@
+<!--Fuzail Shahzad 3-1-2024 IT202-006 Phase 2 Assignment: Read SQL Data using PHP uf7@njit.edu-->
 <html>
     <head>
         <title>QuirkTech</title>
--- database_njit.php
+++ database_njit.php
@@ -1,3 +1,4 @@
+<!--Fuzail Shahzad 3-1-2024 IT202-006 Phase 2 Assignment: Read SQL Data using PHP uf7@njit.edu-->
 <?php
     $dsn= 'mysql:host=sql1.njit.edu;port=3306;dbname=uf7';
     $username='uf7';
